ff7b3cee31bad543c4c9e576510a71727dff7d58
Tyler Firth <tyfirth@gmail.com>
2020-05-15 05:58:21 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,7 +22,7 @@ contacts.each do |person, contact_details_hash|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if
         {|ice_cream| ice_cream == "strawberry"}
-   
+   end
         end
       end
    end
